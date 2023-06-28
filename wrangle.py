1ef3a26c6a3f39350dc1de8f99f6760d3a75b2a3
Oliver k Taylor <o.k.p.taylor@gmail.com>
2023-06-28 09:18:41 -0500

    updated wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
 #play with words
+import unicodedata
+from nltk.corpus import stopwords
 import nltk
 import re
 from pprint import pprint
@@ -16,7 +18,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
 #sql creds
-import env
+
 
 #ignore warnings
 import warnings
@@ -25,21 +27,123 @@ warnings.filterwarnings("ignore")
 """---------------------------------------------------------------------function to get the data---------------------------------------------------------------"""
 
 """---------------------------------------------------------------------function prep the data-----------------------------------------------------------------"""
-def clean(text): #takes the text and cleans it
+#takes a row from the dataframe and cleans it
+
+def basic_clean(original):
+    '''
+    Input: original text or .apply(basic_clean) to entire data frame
+    Actions: 
+    lowercase everything,
+    normalizes everything,
+    removes anything that's not a letter, number, whitespace, or single quote
+    Output: Cleaned text
+    '''
+    # lowercase everything
+    basic_cleaned = original.lower()
+    # normalize unicode characters
+    basic_cleaned = unicodedata.normalize('NFKD', basic_cleaned)\
+    .encode('ascii', 'ignore')\
+    .decode('utf-8')
+    # Replace anything that is not a letter, number, whitespace or a single quote.
+    basic_cleaned = re.sub(r'[^a-z0-9\'\s]', '', basic_cleaned)
     
-    #remove non-ascii characters & lower
-    text = (text.encode('ascii', 'ignore')
-                .decode('utf-8', 'ignore')
-                .lower())
+    return basic_cleaned
+
+def tokenize(basic_cleaned):
+    '''
+    Input: basic_cleaned text string or .apply(tokenize) to entire data frame
+    Actions:
+    creates the tokenizer
+    uses the tokenizer
+    Output: clean_tokenize text string
+    '''
+    #create the tokenizer
+    tokenize = nltk.tokenize.ToktokTokenizer()
+    #use the tokenizer
+    clean_tokenize = tokenize.tokenize(basic_cleaned, return_str=True)
     
-    #remove special characters
+    return clean_tokenize
+
+def remove_stopwords(lemma_or_stem, extra_words=[], exclude_words=[]):
+    '''
+    Input:text string or .apply(remove_stopwords) to entire data frame
+    Action: removes standard stop words
+    Output: parsed_article
+    '''
+    # save stopwords
+    stopwords_ls = stopwords.words('english')
+    # removing any stopwords in exclude list
+    stopwords_ls = set(stopwords_ls) - set(exclude_words)
+    # adding any stopwords in extra list
+    stopwords_ls = stopwords_ls.union(set(extra_words))
+    
+    # split words in article
+    words = lemma_or_stem.split()
+    # remove stopwords from list of words
+    filtered = [word for word in words if word not in stopwords_ls]
+    # join words back together
+    parsed_article = ' '.join(filtered)
+    
+    return parsed_article
+
+def lemmatize(clean_tokenize):
+    '''
+    Inputs: clean_tokenize
+    Actions: creates lemmatizer and applies to each word
+    Outputs: clean_tokenize_lemma
+    '''
+    #create the lemmatizer
+    wnl = nltk.stem.WordNetLemmatizer()
+    #use lemmatize - apply to each word in our string
+    lemmas = [wnl.lemmatize(word) for word in clean_tokenize.split()]
+    #join words back together
+    clean_tokenize_lemma = ' '.join(lemmas)
+    
+    return clean_tokenize_lemma
+
+#splits your data into train, validate, and test sets for cat target var
+def split_function_cat_target(df_name, target_varible_column_name):
+    train, test = train_test_split(df_name,
+                                   random_state=123, #can be whatever you want
+                                   test_size=.20,
+                                   stratify= df_name[target_varible_column_name])
+    
+    train, validate = train_test_split(train,
+                                   random_state=123,
+                                   test_size=.25,
+                                   stratify= train[target_varible_column_name])
+    return train, validate, test
+
+def clean(text):
+    '''
+    A simple function to cleanup text data.
+    
+    Args:
+        text (str): The text to be cleaned.
+        
+    Returns:
+        list: A list of lemmatized words after cleaning.
+    '''
+    
+    # basic_clean() function from last lesson:
+    # Normalize text by removing diacritics, encoding to ASCII, decoding to UTF-8, and converting to lowercase
+    text = (unicodedata.normalize('NFKD', text)
+             .encode('ascii', 'ignore')
+             .decode('utf-8', 'ignore')
+             .lower())
+    
+    # Remove punctuation, split text into words
     words = re.sub(r'[^\w\s]', '', text).split()
     
-    #build the lemmatizer
+    
+    # lemmatize() function from last lesson:
+    # Initialize WordNet lemmatizer
     wnl = nltk.stem.WordNetLemmatizer()
     
-    #getting all stopwords
-    stopwords = set(nltk.corpus.stopwords.words('english'))
+    # Combine standard English stopwords with additional stopwords
+    stopwords = nltk.corpus.stopwords.words('english')
     
-    return [wnl.lemmatize(word) for word in words if word not in stopwords]
-
+    # Lemmatize words and remove stopwords
+    cleaned_words = [wnl.lemmatize(word) for word in words if word not in stopwords]
+    
+    return cleaned_words
